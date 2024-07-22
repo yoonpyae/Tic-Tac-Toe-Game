fe202a8ea2b2677b9001753bd3cce681c591095e
yoonpyae <yoonpyae26@gmail.com>
2024-07-22 23:24:15 +0630

    fix color error

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ let turn = "X";
 let isGameOver = false;
 
 function updateTurnIndicator() {
-    if (turn === "X") {
-      document.getElementById("turn-x").classList.add("active");
-      document.getElementById("turn-o").classList.remove("active");
-    } else {
-      document.getElementById("turn-x").classList.remove("active");
-      document.getElementById("turn-o").classList.add("active");
-    }
+  if (turn === "X") {
+    document.getElementById("turn-x").classList.add("active");
+    document.getElementById("turn-o").classList.remove("active");
+  } else {
+    document.getElementById("turn-x").classList.remove("active");
+    document.getElementById("turn-o").classList.add("active");
   }
+}
 
-boxes.forEach(e => {
+boxes.forEach((e) => {
   e.innerHTML = "";
   e.addEventListener("click", () => {
     if (!isGameOver && e.innerHTML === "") {
@@ -25,15 +25,20 @@ boxes.forEach(e => {
 });
 
 function changeTurn() {
-    turn = turn === "X" ? "O" : "X";
-    updateTurnIndicator();
-  }
+  turn = turn === "X" ? "O" : "X";
+  updateTurnIndicator();
+}
 
 function checkWin() {
   let winContainer = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
   for (let i = 0; i < winContainer.length; i++) {
     let vo = boxes[winContainer[i][0]].innerHTML;
@@ -47,7 +52,7 @@ function checkWin() {
 
       for (j = 0; j < 3; j++) {
         boxes[winContainer[i][j]].style.backgroundColor = "#08d9d6";
-        boxes[winContainer[i][j]].style.color = "#0000";
+        boxes[winContainer[i][j]].style.color = "#fff";
       }
     }
   }
@@ -56,7 +61,7 @@ function checkWin() {
 function checkDraw() {
   if (!isGameOver) {
     let isDraw = true;
-    boxes.forEach(e => {
+    boxes.forEach((e) => {
       if (e.innerHTML === "") isDraw = false;
     });
 
@@ -69,18 +74,18 @@ function checkDraw() {
 }
 
 document.querySelector("#play-again").addEventListener("click", () => {
-    isGameOver = false;
-    turn = "X";
-    updateTurnIndicator();
-    document.querySelector("#results").innerHTML = "";
-    document.querySelector("#play-again").style.display = "none";
-  
-    boxes.forEach(e => {
-      e.innerHTML = "";
-      e.style.removeProperty("background-color");
-      e.style.color = "#fff";
-    });
-  });
-  
-  // Initialize the turn indicator
+  isGameOver = false;
+  turn = "X";
   updateTurnIndicator();
+  document.querySelector("#results").innerHTML = "";
+  document.querySelector("#play-again").style.display = "none";
+
+  boxes.forEach((e) => {
+    e.innerHTML = "";
+    e.style.removeProperty("background-color");
+    e.style.color = "#fff";
+  });
+});
+
+// Initialize the turn indicator
+updateTurnIndicator();
